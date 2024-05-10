3688fcc0a8a24e0e0b4219ac300bf44846f8ac9c
Abdur Rohman <masabdrohman@gmail.com>
2024-05-10 15:55:12 +0700

    Update integral_numerik.R

--- integral_numerik.R
+++ integral_numerik.R
@@ -3,24 +3,17 @@ fkuadrat = function(x) -2 * x ** 2 + 3 * x + 7
 fsinus = function(x) sin(x * pi/180)
 flogn = function(x) log(x + 1)
 
-integral_kotak = function(namafungsi,
-                          batas_kiri,
-                          batas_kanan,
-                          banyak_kotak) {
+integral_kotak = function(namafungsi, batas_kiri, batas_kanan, banyak_kotak) {
   lebar_kotak = (batas_kanan - batas_kiri) / banyak_kotak
   luas_tiap_kotak = rep(0, banyak_kotak)
   for (i in 1:banyak_kotak) {
-    luas_tiap_kotak[i] =
-      namafungsi(batas_kiri + (i - 1) * lebar_kotak) * lebar_kotak
+    luas_tiap_kotak[i] = namafungsi(batas_kiri + (i - 1) * lebar_kotak) * lebar_kotak
   }
   hasil_integral = sum(luas_tiap_kotak)
   return(hasil_integral)
 }
 
-integral_trapesium = function(namafungsi,
-                              batas_kiri,
-                              batas_kanan,
-                              banyak_trapesium) {
+integral_trapesium = function(namafungsi, batas_kiri, batas_kanan, banyak_trapesium) {
   lebar = (batas_kanan - batas_kiri) / banyak_trapesium
   y_kiri = namafungsi(batas_kiri)
   y_kanan = namafungsi(batas_kanan)
